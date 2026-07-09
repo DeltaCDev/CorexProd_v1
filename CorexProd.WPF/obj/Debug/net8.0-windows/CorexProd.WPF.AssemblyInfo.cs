e9bc04941f01291aa2b2332d097befc39f3cbb59
DeltaCDev <deltaconfecciones.dev@outlook.com>
2026-07-09 08:58:37 -0500

    1

--- CorexProd.WPF/obj/Debug/net8.0-windows/CorexProd.WPF.AssemblyInfo.cs
+++ CorexProd.WPF/obj/Debug/net8.0-windows/CorexProd.WPF.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CorexProd.WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64dbd4cfba5b99c98a3e3b9573e988f491455199")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49d338140e10084cfa88d05fcde8344523962725")]
 [assembly: System.Reflection.AssemblyProductAttribute("CorexProd.WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CorexProd.WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
